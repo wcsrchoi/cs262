00fd5cbd8e54a1eea22268834889461d8d9bc5ec
wcsrchoi <wcsrchoi@gmail.com>
2022-10-13 15:07:40 -0400

    commit

---
+++ Homework2/README.md
@@ -0,0 +1,12 @@
+CS 262 - Lab 5
+This lab is based on https://cs.calvin.edu/courses/cs/262/kvlinden/05design/lab.html
+
+Answers to the lab questions:
+1. Of what benefit was the refactoring phase of this lab?
+--> Organization and grouping similar items together.
+
+2. What do the export commands do?
+--> You export so that we don't have to rewrite functions everytime we use them. Basically export to import.
+
+3. This application uses stack navigation. What does the stack do here? What is the deepest this stack can get in this application?
+--> Stack keeps track on where you are and where you previously were. This is 1 layer deep stack.
---
+++ Homework2/my-app/screens/home.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { globalStyles } from 'my-app/styles/global';
+
+export default function HomeScreen({ navigation }) {
+
+  /* Hardcode a "database"/list of movies. */
+  const [reviews, setReviews] = useState([
+    {
+      title: "Fellowship of the Ring", rating: 8.8, key: '1',
+      description: "Some hobbits begin a quest."
+    },
+    {
+      title: "Two Towers", rating: 8.7, key: '2',
+      description: 'Some dark lords try to take over the world.'
+    },
+    {
+      title: "Return of the King", rating: 8.9, key: '3',
+      description: 'The dark lords are defeated, to much rejoicing.'
+    },
+  ]);
+
+  return (
+    <View style={globalStyles.container}>
+      {/* Get rid of that ugly button and, instead, display our list of movies. */}
+      <FlatList data={reviews} renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigation.navigate('Details', item)}>
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+      )} />
+    </View>
+  );
+}
