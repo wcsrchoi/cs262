cc003b9273dbe47dcfcf59ed3abfa8a2770462df
wcsrchoi <wcsrchoi@gmail.com>
2022-10-30 02:02:56 -0400

    commit

--- lab09/sql/monopoly.sql
+++ lab09/sql/monopoly.sql
@@ -9,38 +9,48 @@
 DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS Properties;
 
 -- Create the schema.
 CREATE TABLE Game (
-                      ID SERIAL PRIMARY KEY,
-                      time timestamp
+	ID integer PRIMARY KEY,
+	time timestamp
 );
 
 CREATE TABLE Player (
-                        ID SERIAL PRIMARY KEY,
-                        email varchar(50) NOT NULL,
-                        name varchar(50)
+	ID integer PRIMARY KEY,
+	emailAddress varchar(50) NOT NULL,
+	name varchar(50)
 );
 
 CREATE TABLE PlayerGame (
-                            gameID integer REFERENCES Game(ID),
-                            playerID integer REFERENCES Player(ID),
-                            score integer
+	gameID integer REFERENCES Game(ID),
+	playerID integer REFERENCES Player(ID),
+	score integer
+);
+
+CREATE TABLE Properties (
+	propertiesID integer REFERENCES player(ID),
+	cash numeric(50),
+	houses numeric(50),
+	hotels numeric(50),
+	piece numeric(50)
 );
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
+GRANT SELECT ON Properties TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO Game(time) VALUES ('2006-06-27 08:00:00');
-INSERT INTO Game(time) VALUES ('2006-06-28 13:20:00');
-INSERT INTO Game(time) VALUES ('2006-06-29 18:41:00');
+INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
+INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
+INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
 
-INSERT INTO Player(email) VALUES ('me@calvin.edu');
-INSERT INTO Player(email, name) VALUES ('king@gmail.edu', 'The King');
-INSERT INTO Player(email, name) VALUES ('dog@gmail.edu', 'Dogbreath');
+INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
+INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
+INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
 
 INSERT INTO PlayerGame VALUES (1, 1, 0.00);
 INSERT INTO PlayerGame VALUES (1, 2, 0.00);
@@ -50,3 +60,7 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+
+INSERT INTO Properties VALUES (1, 421, 2, 5, 5);
+INSERT INTO Properties VALUES (2, 399, 3, 1, 7);
+INSERT INTO Properties VALUES (3, 256, 1, 8, 3);
