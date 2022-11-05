1f66f5d7e649371c1cc3db870d77dabc81fc8e26
wcsrchoi <wcsrchoi@gmail.com>
2022-11-05 00:50:27 -0400

    commit

--- Homework3/service/sql/monopoly-queries.sql
+++ Homework3/service/sql/monopoly-queries.sql
@@ -11,14 +11,14 @@ SELECT *
   ;
 
 -- Get the player records.
-SELECT *
+SELECT * 
   FROM Player
   ;
 
 -- Get all the users with Calvin email addresses.
 SELECT *
   FROM Player
- WHERE emailAddress LIKE '%calvin%'
+ WHERE email LIKE '%calvin%'
  ;
 
 -- Get the highest score ever recorded.
@@ -30,71 +30,5 @@ ORDER BY score DESC
 
 -- Get the cross-product of all the tables.
 SELECT *
-  FROM Player, PlayerGame, Game, Properties
+  FROM Player, PlayerGame, Game
   ;
-
--- ----------------------------------------------------------------------------------------------------------------
--- Exercise 8.1
-
--- a.
--- SELECT *
---   FROM Game
---   ORDER BY time DESC
---   ;
-
--- b.
--- SELECT *
---   From Game
---   WHERE time > '2022-10-14'
---   ;
-
--- c.
--- SELECT *
---   FROM Player
---   WHERE name IS NOT NULL
---   ;
-
--- d.
--- SELECT playerID
---   FROM PlayerGame
---   WHERE score > 2000
---   ;
-
--- e.
--- SELECT *
---   FROM Player
---   WHERE emailAddress LIKE '%gmail%'
---   ;
-
--- ----------------------------------------------------------------------------------------------------------------
--- Excercise 8.2
-
--- a.
--- SELECT score
---   FROM Player, PlayerGame
---   WHERE Player.name = 'The King'
---   ORDER BY PlayerGame.score DESC
---   ;
-
--- b.
--- Select *
---   FROM Game, Player, PlayerGame
---   WHERE Game.time = '2006-06-28 13:20:00'
---   AND Game.ID = PlayerGame.GameID
---   AND Player.ID = PlayerGame.PlayerID
---   ORDER BY score DESC
---   LIMIT 1
---   ;
-
--- c/
--- Select players with same name and has lower ID
-
--- d.
--- Probably when data in the table needs another data that is closely related.
--- Such as a day and a month in a birth date.
-
-
-
-
-
-
--- Homework3/service/sql/monopoly.sql
+++ Homework3/service/sql/monopoly.sql
@@ -9,48 +9,38 @@
 DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
-DROP TABLE IF EXISTS Properties;
 
 -- Create the schema.
 CREATE TABLE Game (
-	ID integer PRIMARY KEY,
-	time timestamp
+                      ID SERIAL PRIMARY KEY,
+                      time timestamp
 );
 
 CREATE TABLE Player (
-	ID integer PRIMARY KEY,
-	emailAddress varchar(50) NOT NULL,
-	name varchar(50)
+                        ID SERIAL PRIMARY KEY,
+                        email varchar(50) NOT NULL,
+                        name varchar(50)
 );
 
 CREATE TABLE PlayerGame (
-	gameID integer REFERENCES Game(ID),
-	playerID integer REFERENCES Player(ID),
-	score integer
-);
-
-CREATE TABLE Properties (
-	propertiesID integer REFERENCES player(ID),
-	cash numeric(50),
-	houses numeric(50),
-	hotels numeric(50),
-	piece numeric(50)
+                            gameID integer REFERENCES Game(ID),
+                            playerID integer REFERENCES Player(ID),
+                            score integer
 );
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
-GRANT SELECT ON Properties TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
-INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
-INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
+INSERT INTO Game(time) VALUES ('2006-06-27 08:00:00');
+INSERT INTO Game(time) VALUES ('2006-06-28 13:20:00');
+INSERT INTO Game(time) VALUES ('2006-06-29 18:41:00');
 
-INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
-INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
-INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
+INSERT INTO Player(email) VALUES ('me@calvin.edu');
+INSERT INTO Player(email, name) VALUES ('king@gmail.edu', 'The King');
+INSERT INTO Player(email, name) VALUES ('dog@gmail.edu', 'Dogbreath');
 
 INSERT INTO PlayerGame VALUES (1, 1, 0.00);
 INSERT INTO PlayerGame VALUES (1, 2, 0.00);
@@ -60,7 +50,3 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
-
-INSERT INTO Properties VALUES (1, 421, 2, 5, 5);
-INSERT INTO Properties VALUES (2, 399, 3, 1, 7);
-INSERT INTO Properties VALUES (3, 256, 1, 8, 3);
